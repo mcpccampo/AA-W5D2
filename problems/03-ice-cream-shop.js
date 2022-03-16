b984ac5ae605404e678ecf3d45426aebe2934a6d
Camilo Campo <mcp.ccampo@gmail.com>
2022-03-15 23:27:21 -0400

    problem 3/4

--- problems/03-ice-cream-shop.js
+++ problems/03-ice-cream-shop.js
@@ -12,12 +12,16 @@ iceCreamShop(['moose tracks'], 'moose tracks'); // true
 iceCreamShop([], 'honey lavender'); // false
 ***********************************************************************/
 
-
 function iceCreamShop(flavors, favorite) {
-  // Your code here
+  if (!flavors.length) return false
+  if (flavors.pop() === favorite) return true
+  return iceCreamShop(flavors,favorite)
 }
-
-
+console.log(iceCreamShop(['vanilla', 'strawberry'], 'blue moon')); // false
+console.log(iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')); // true
+iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio'); // false
+iceCreamShop(['moose tracks'], 'moose tracks'); // true
+iceCreamShop([], 'honey lavender'); // false
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = iceCreamShop;
